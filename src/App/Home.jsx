23daac357e576a2faae106e1954c943484f9a0e5
Sarah Ohlin <sarahohlin1@gmail.com>
2019-09-30 14:25:35 -0400

    added greeting page

--- src/App/Home.jsx
+++ src/App/Home.jsx
@@ -10,6 +10,7 @@ export default class Home extends Component
 		super(props);
 		
 		this.state = {
+			started: false
 		};
 	}
 	
@@ -20,6 +21,34 @@ export default class Home extends Component
 	
 	render()
 	{
+		if(!this.state.started)
+		{
+			return (
+			<>
+				<UI.AppBar position="static">
+					<UI.Grid container alignItems="center" justify="center" direction="row">
+						<UI.Grid item>
+							<UI.Toolbar>
+								<UI.Typography variant="h6" align="center">
+									Am I A Cat?
+								</UI.Typography>
+							</UI.Toolbar>
+						</UI.Grid>
+					</UI.Grid>
+				</UI.AppBar>
+				<UI.Grid container justify="center" align="center" direction="column" style={{height: '100%'}}>
+					<UI.Grid item style={{height: '100%'}}>
+						<UI.Paper style={{marginTop: '10%', padding: '20px', width: '60%', height: '40%'}}>
+							<UI.Typography variant="h3">
+								Welcome to the "Are you a cat?" quiz!
+							</UI.Typography>
+							<UI.Button onClick={() => this.setState({started: true})}>Start!</UI.Button>
+						</UI.Paper>
+					</UI.Grid>
+				</UI.Grid>
+			</>	
+			);
+		}
 		return (
 			<>
 				<UI.AppBar position="static">
